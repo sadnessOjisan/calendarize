f88677b49c0cf4cd4f38e2da798f11ab3ca1a4bd
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-08 03:31:17 +0900

    get last date of month

--- src/calendrize.rs
+++ src/calendrize.rs
@@ -1,17 +1,22 @@
-use chrono::NaiveDate;
+use chrono::*;
 
-pub fn calendrize(date: NaiveDate) -> NaiveDate{
+pub fn calendrize(date: NaiveDate) -> (NaiveDate, u32){
     println!("------------");
-    return date;
+    let year = date.year(); 
+    let month = date.month();
+    let first = NaiveDate::from_ymd(year, month, 1);
+    let days = NaiveDate::from_ymd_opt(year, month + 1, 1).unwrap_or(NaiveDate::from_ymd(year + 1, 1, 1)).pred().day();
+    println!("{}",days);
+    
+    return (first, days);
 }
 
 mod tests {
-    use chrono::NaiveDate;
     use super::*;
     #[test]
     fn it_works() {
-        let date = NaiveDate::parse_from_str("2018-12-07", "%Y-%m-%d").unwrap();
+        let date = NaiveDate::parse_from_str("2018-11-07", "%Y-%m-%d").unwrap();
         let actual = calendrize(date);
-        assert_eq!(NaiveDate::parse_from_str("2018-12-01", "%Y-%m-%d").unwrap(), actual);
+        assert_eq!((NaiveDate::parse_from_str("2018-12-01", "%Y-%m-%d").unwrap(), 12), actual);
     }
 }
