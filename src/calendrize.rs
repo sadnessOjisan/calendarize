0b9af110d3c49eabf645b799007954cfaae33003
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-09 01:59:26 +0900

    pass all test

--- src/calendrize.rs
+++ src/calendrize.rs
@@ -7,10 +7,6 @@ pub fn calendrize(date: NaiveDate) -> Vec<[u32; 7]> {
     let mut first_date_day = NaiveDate::from_ymd(year, month, 1)
         .weekday()
         .num_days_from_sunday();
-    println!(
-        "weekday: {:?}",
-        NaiveDate::from_ymd(year, month, 1).weekday()
-    );
     let end_date = NaiveDate::from_ymd_opt(year, month + 1, 1)
         .unwrap_or(NaiveDate::from_ymd(year + 1, 1, 1))
         .pred()
@@ -20,11 +16,9 @@ pub fn calendrize(date: NaiveDate) -> Vec<[u32; 7]> {
     loop {
         let mut day = 0 as usize;
         week = [0; 7];
-        println!("first_date_day: {:?}", first_date_day);
         loop {
-            println!("day: {:?}", day);
             if day < first_date_day as usize {
-                println!("no");
+                // no op
             } else if day == 7 {
                 first_date_day = 0;
                 break;
@@ -33,8 +27,8 @@ pub fn calendrize(date: NaiveDate) -> Vec<[u32; 7]> {
                 week[day] = date;
                 first_date_day = 0;
                 if date >= end_date {
-            break;
-        }
+                    break;
+                }
             };
             day = day + 1;
         }
@@ -66,51 +60,51 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn april() {
-    //     let date = NaiveDate::parse_from_str("2021-02-02", "%Y-%m-%d").unwrap();
-    //     let actual = calendrize(date);
-    //     assert_eq!(
-    //         vec![
-    //             [0, 0, 0, 0, 1, 2, 3],
-    //             [4, 5, 6, 7, 8, 9, 10],
-    //             [11, 12, 13, 14, 15, 16, 17],
-    //             [18, 19, 20, 21, 22, 23, 24],
-    //             [25, 26, 27, 28, 29, 30, 0]
-    //         ],
-    //         actual
-    //     );
-    // }
+    #[test]
+    fn april() {
+        let date = NaiveDate::parse_from_str("2021-04-02", "%Y-%m-%d").unwrap();
+        let actual = calendrize(date);
+        assert_eq!(
+            vec![
+                [0, 0, 0, 0, 1, 2, 3],
+                [4, 5, 6, 7, 8, 9, 10],
+                [11, 12, 13, 14, 15, 16, 17],
+                [18, 19, 20, 21, 22, 23, 24],
+                [25, 26, 27, 28, 29, 30, 0]
+            ],
+            actual
+        );
+    }
 
-    // #[test]
-    // fn uruudoshi() {
-    //     let date = NaiveDate::parse_from_str("2020-02-02", "%Y-%m-%d").unwrap();
-    //     let actual = calendrize(date);
-    //     assert_eq!(
-    //         vec![
-    //             [0, 0, 0, 0, 0, 0, 1],
-    //             [2, 3, 4, 5, 6, 7, 8],
-    //             [9, 10, 11, 12, 13, 14, 15],
-    //             [16, 17, 18, 19, 20, 21, 22],
-    //             [23, 24, 25, 26, 27, 28, 29]
-    //         ],
-    //         actual
-    //     );
-    // }
+    #[test]
+    fn uruudoshi() {
+        let date = NaiveDate::parse_from_str("2020-02-02", "%Y-%m-%d").unwrap();
+        let actual = calendrize(date);
+        assert_eq!(
+            vec![
+                [0, 0, 0, 0, 0, 0, 1],
+                [2, 3, 4, 5, 6, 7, 8],
+                [9, 10, 11, 12, 13, 14, 15],
+                [16, 17, 18, 19, 20, 21, 22],
+                [23, 24, 25, 26, 27, 28, 29]
+            ],
+            actual
+        );
+    }
 
-    // #[test]
-    // fn uruwanaidoshi() {
-    //     let date = NaiveDate::parse_from_str("2021-02-02", "%Y-%m-%d").unwrap();
-    //     let actual = calendrize(date);
-    //     assert_eq!(
-    //         vec![
-    //             [0, 1, 2, 3, 4, 5, 6],
-    //             [7, 8, 9, 10, 11, 12, 13],
-    //             [14, 15, 16, 17, 18, 19, 20],
-    //             [21, 22, 23, 24, 25, 26, 27],
-    //             [28, 0, 0, 0, 0, 0, 0]
-    //         ],
-    //         actual
-    //     );
-    // }
+    #[test]
+    fn uruwanaidoshi() {
+        let date = NaiveDate::parse_from_str("2021-02-02", "%Y-%m-%d").unwrap();
+        let actual = calendrize(date);
+        assert_eq!(
+            vec![
+                [0, 1, 2, 3, 4, 5, 6],
+                [7, 8, 9, 10, 11, 12, 13],
+                [14, 15, 16, 17, 18, 19, 20],
+                [21, 22, 23, 24, 25, 26, 27],
+                [28, 0, 0, 0, 0, 0, 0]
+            ],
+            actual
+        );
+    }
 }
