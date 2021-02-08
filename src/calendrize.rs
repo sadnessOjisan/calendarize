7f7eeec446759c95050b3c2460645cdcb21b8ce8
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-08 23:14:52 +0900

    add test case

--- src/calendrize.rs
+++ src/calendrize.rs
@@ -41,9 +41,30 @@ pub fn calendrize(date: NaiveDate) -> Vec<[u32; 7]>{
 mod tests {
     use super::*;
     #[test]
-    fn it_works() {
-        let date = NaiveDate::parse_from_str("2018-11-07", "%Y-%m-%d").unwrap();
+    fn january() {
+        let date = NaiveDate::parse_from_str("2021-01-02", "%Y-%m-%d").unwrap();
         let actual = calendrize(date);
-        assert_eq!(vec![[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]], actual);
+        assert_eq!(vec![[0,0,0,0,0,1,2],[3,4,5,6,7,8,9],[10,11,12,13,14,15,16],[17,18,19,20,21,22,23],[24,25,26,27,28,29,30],[31,0,0,0,0,0,0]], actual);
+    }
+
+    #[test]
+    fn april() {
+        let date = NaiveDate::parse_from_str("2021-02-02", "%Y-%m-%d").unwrap();
+        let actual = calendrize(date);
+        assert_eq!(vec![[0,0,0,0,1,2,3],[4,5,6,7,8,9,10],[11,12,13,14,15,16,17],[18,19,20,21,22,23,24],[25,26,27,28,29,30,0]], actual);
+    }
+
+   #[test]
+    fn uruwanaidoshi() {
+        let date = NaiveDate::parse_from_str("2021-02-02", "%Y-%m-%d").unwrap();
+        let actual = calendrize(date);
+        assert_eq!(vec![[0,1,2,3,4,5,6],[7,8,9,10,11,12,13],[14,15,16,17,18,19,20],[21,22,23,24,25,26,27],[28,0,0,0,0,0,0]], actual);
+    }
+
+    #[test]
+    fn uruudoshi() {
+        let date = NaiveDate::parse_from_str("2020-02-02", "%Y-%m-%d").unwrap();
+        let actual = calendrize(date);
+        assert_eq!(vec![[0,0,0,0,0,0,1],[2,3,4,5,6,7,8],[9,10,11,12,13,14,15],[16,17,18,19,20,21,22],[23,24,25,26,27,28,29]], actual);
     }
 }
